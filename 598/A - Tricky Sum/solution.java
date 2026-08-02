b8288065d4266c35dc1cb11a6d7c9abe40108792
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:37:28 +0530

    Add Tricky Sum [A] from Codeforces

---
+++ 598/A - Tricky Sum/solution.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = in.nextInt();
+ 
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        long n = in.nextLong();
+ 
+        long totalSum = (n*(n+1))/2;
+ 
+        long twoPowerSum = 0;
+ 
+        int i = 0;
+        while(true) {
+            long num = (long)(Math.pow(2,i));
+            if(num<=n) twoPowerSum += num;
+            else break;
+            i++;
+        }
+ 
+        totalSum -= 2*twoPowerSum;
+ 
+        out.println(totalSum);
+ 
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
