9ef6393f76c31ebb01dec6cfa7a65c00e35931d3
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:14:29 +0530

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(){
+    
+    int a , b;
+    cin >> a >> b;
+    cout<<(b-a)<<"
+";
+ 
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n ;
+    cin >> n;
+    while(n){
+        solve();
+        n--;
+    }
+    return 0;
+}
