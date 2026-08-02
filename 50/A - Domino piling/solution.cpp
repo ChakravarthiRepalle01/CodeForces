dc36424c118b5db94bbe8b45cc011469281a0a57
ChakravarthiRepalle01 <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:00:13 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int m,n;
+    cin >> m>> n;
+    
+    int totalBoxes = m*n;
+    
+    int ans = totalBoxes/2;
+    
+    cout<<ans<<"
+";
+    
+    return 0;
+    
+}
