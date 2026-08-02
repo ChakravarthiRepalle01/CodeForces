f59270911154853c5a05d5706f9da90c7166d0d9
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:19:22 +0530

    Add Easy Problem [A] from Codeforces

---
+++ 2044/A - Easy Problem/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        while(n>0) {
+            solve(sc);
+            n--;
+        }
+        sc.close();
+    }
+    
+    public static void solve(Scanner sc) {
+        int n = sc.nextInt();
+        
+        System.out.println((n-1));
+        
+    }
+}
