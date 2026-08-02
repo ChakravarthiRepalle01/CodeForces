7a9d3a3717a27754de5e0d9819cb19833765ff33
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:22:44 +0530

    Add Colourblindness [B] from Codeforces

---
+++ 1722/B - Colourblindness/solution.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        
+        // int t = 1;
+        int t = in.nextInt();
+        
+        while(t > 0) {
+            solve(in, out);
+            t--;
+        }
+        out.flush(); 
+    }
+    
+    public static void solve(FastReader in, PrintWriter out) {
+        int n = in.nextInt();
+        String s1 = in.next();
+        String s2 = in.next();
+        
+        for(int i = 0 ; i<n ; i++) {
+            if((s1.charAt(i) == 'R' && s2.charAt(i) != 'R') || (s2.charAt(i) == 'R' && s1.charAt(i) != 'R')) {
+                out.println("NO");
+                return;
+            }
+        }
+        out.println("YES");
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                if (st != null && st.hasMoreTokens()) {
+                    str = st.nextToken("
+");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
