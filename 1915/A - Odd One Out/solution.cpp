a779f7abbbbad2d65cbd6499ee0562ae98465ef9
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:15:35 +0530

    Add Odd One Out [A] from Codeforces

---
+++ 1915/A - Odd One Out/solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(){
+    int a , b , c;
+    cin >> a >> b >> c;
+    cout<<(a^(b^c))<<"
+";
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n ;
+    cin >> n;
+    while(n){
+        solve();
+        n--;
+    }
+    return 0;
+}
