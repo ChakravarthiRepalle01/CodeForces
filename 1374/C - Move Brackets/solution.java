4a5c6b3ef87b85dcc1b1c8706fd82e78750df724
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:22:40 +0530

    Add Move Brackets [C] from Codeforces

---
+++ 1374/C - Move Brackets/solution.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        
+        // int t = 1;
+        int t = in.nextInt();
+        
+        while(t > 0) {
+            solve(in, out);
+            t--;
+        }
+        out.flush(); 
+    }
+    
+    public static void solve(FastReader in, PrintWriter out) {
+        int n = in.nextInt();
+        
+        String s = in.next();
+        
+        int open = 0;
+        int totalCnt = 0;
+        
+        for(int i = 0 ; i<n ; i++) {
+            if(s.charAt(i) == '(') {
+                open++;
+            }
+            else {
+                if(open>0) open--;
+                else totalCnt++;
+            }
+        }
+        out.println(totalCnt);
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                if (st != null && st.hasMoreTokens()) {
+                    str = st.nextToken("
+");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
