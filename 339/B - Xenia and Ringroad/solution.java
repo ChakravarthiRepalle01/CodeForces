500ee57bb9b29b519182746a8fd9a35c8f109a9a
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:21:11 +0530

    Add Xenia and Ringroad [B] from Codeforces

---
+++ 339/B - Xenia and Ringroad/solution.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        
+        int t = 1;
+        // int t = in.nextInt();
+        
+        while(t > 0) {
+            solve(in, out);
+            t--;
+        }
+        out.flush(); 
+    }
+    
+    public static void solve(FastReader in, PrintWriter out) {
+        int n = in.nextInt();
+        int m = in.nextInt();
+        
+        int arr[] = new int[m];
+        
+        for(int i = 0 ; i<m ; i++) {
+            arr[i] = in.nextInt();
+        }
+        
+        int pos = 1;
+        long totalMoves = 0;
+        
+        for(int i = 0 ; i<m ; i++) {
+            if(pos > arr[i]) {
+                totalMoves += (n+arr[i]-pos);
+            }
+            else {
+                totalMoves += (arr[i]-pos);
+            }
+            pos = arr[i];
+        }
+        out.println(totalMoves);
+    }
+ 
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                if (st != null && st.hasMoreTokens()) {
+                    str = st.nextToken("
+");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
