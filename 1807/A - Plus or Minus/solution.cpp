ec31ddce372015d784f9e46ed8e4ca9008927a99
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:12:59 +0530

    Add Plus or Minus [A] from Codeforces

---
+++ 1807/A - Plus or Minus/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(){
+    int a , b , c;
+    cin >> a >> b >> c;
+    if(a+b==c) cout<<"+"<<"
+";
+    else cout<<"-"<<"
+";
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    while(n){
+        solve();
+        n--;
+    }
+    return 0;
+}
