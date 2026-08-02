583df32ea4f7fb4ce07c0d4bd82dbffe0ca3d79e
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-02 21:13:55 +0530

    Add Candies and Two Sisters [A] from Codeforces

---
+++ 1335/A - Candies and Two Sisters/solution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(){
+    
+    long long n;
+    cin >> n;
+    
+    cout<<(n-1)/2<<"
+";
+    
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    while(n){
+        solve();
+        n--;
+    }
+    return 0;
+}
