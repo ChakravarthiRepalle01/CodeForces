061d8a2f82edc4b37f6c3cb624bec3a46fa3d6fd
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-09 21:49:10 +0530

    Add Buying Shovels [D] from Codeforces

---
+++ 1360/D - Buying Shovels/solution.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    static int arr[] = new int[2*(int)Math.pow(10,5)+1];
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        int t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+ 
+        int minPackages = n;
+ 
+        for(int i = 1 ; i*i<=n ; i++) {
+            if(n%i == 0) {
+                if(i<=k) {
+                    minPackages = Math.min(minPackages , n/i);
+                }
+                if(n/i <= k) {
+                    minPackages = Math.min(minPackages , i);
+                }
+            }
+        }
+        out.println(minPackages);
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
