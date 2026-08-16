77a50f3ef7c4e177cbd22ec7afe1a55c61b96df3
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-16 08:50:00 +0530

    Add New Year Candles [A] from Codeforces

---
+++ 379/A - New Year Candles/solution.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        // int t = in.nextInt();
+        int t = 1;
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int a = in.nextInt();
+        int b = in.nextInt();
+ 
+        int totalLight = a;
+        int totalWent = 0;
+        long totalHours = 0;
+ 
+        while(totalLight > 0) {
+            totalHours += totalLight;
+            totalWent += totalLight;
+            totalLight = totalWent/b;
+            totalWent -= totalLight*b;
+        }
+        
+        out.println(totalHours);
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
