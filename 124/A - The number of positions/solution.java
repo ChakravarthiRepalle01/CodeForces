8494bdca3983be7d508d815f69cc9d818cf442f0
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-17 19:31:44 +0530

    Add The number of positions [A] from Codeforces

---
+++ 124/A - The number of positions/solution.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        // int t = in.nextInt();
+        int t = 1;
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+ 
+        out.println(n - Math.max(a+1 , n-b)+1);
+ 
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
