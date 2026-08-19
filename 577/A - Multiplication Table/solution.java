61aba69d730861d7a642dce8f0f433e2f71836e5
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-19 16:00:00 +0530

    Add Multiplication Table [A] from Codeforces

---
+++ 577/A - Multiplication Table/solution.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        int t = 1;
+        // t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+        int x = in.nextInt();
+ 
+        long totalCnt = 0;
+ 
+        for(int i = 1 ; i<=n ; i++) {
+            int j = x/i;
+            if(i<=n && j<=n && i*j == x) totalCnt++;
+        }
+ 
+        out.println(totalCnt);
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
