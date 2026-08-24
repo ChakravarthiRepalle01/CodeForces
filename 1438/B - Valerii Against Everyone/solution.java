94b8f36959b62de75ad061a76613c0d838484f3a
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-24 14:06:24 +0530

    Add Valerii Against Everyone [B] from Codeforces

---
+++ 1438/B - Valerii Against Everyone/solution.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        int t = 1;
+        t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+ 
+        int arr[] = new int[n];
+ 
+        for(int i = 0 ; i<n ; i++) {
+            arr[i] = in.nextInt();
+        }
+ 
+        HashSet<Integer> set = new HashSet<Integer>();
+ 
+        for(int i = 0 ; i<n ; i++) {
+            if(set.contains(arr[i])) {
+                out.println("YES");
+                return;
+            }
+            set.add(arr[i]);
+        }  
+        out.println("NO");
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
