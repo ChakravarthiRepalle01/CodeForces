7569d1aeab9eef9a2ba27626f3c5dd64b9c98c0d
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-26 21:28:20 +0530

    Add Omkar and Last Class of Math [B] from Codeforces

---
+++ 1372/B - Omkar and Last Class of Math/solution.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        int t = 1;
+        t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+ 
+        for(int i = 2 ; i*i<=n ; i++) {
+            if(n%i == 0) {
+                out.println(n/i + " " + (n-n/i));
+                return;
+            }
+        }
+        out.println(1 + " " + (n-1));
+ 
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
