cef64c62782c92ac48c03f2f451da23fe4c643fb
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-30 16:06:12 +0530

    Add Registration System [C] from Codeforces

---
+++ 4/C - Registration System/solution.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        int t = 1;
+        // t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+ 
+        HashMap<String , Integer> map = new HashMap<>();
+ 
+        for(int i = 0 ; i<n ; i++) {
+            String s = in.next();
+            if(map.containsKey(s)) {
+                int val = map.get(s);
+                StringBuilder sb = new StringBuilder(s);
+                sb.append(String.valueOf(val));
+                out.println(sb.toString());
+            }
+            else {
+                out.println("OK");
+            }
+            map.merge(s , 1 , Integer::sum);
+        }
+ 
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
