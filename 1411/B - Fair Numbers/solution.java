e8c2f535fe6d85b937c8ce63b522f64ccd2dd8ff
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-31 19:42:30 +0530

    Add Fair Numbers [B] from Codeforces

---
+++ 1411/B - Fair Numbers/solution.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+ 
+        int t = 1;
+        t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        long n = in.nextLong();
+        long validNum = 0;
+        while(true) {
+            long temp = n;
+            boolean isDivisible = true;
+            while(temp > 0) {
+                long k = temp%10;
+                temp /= 10;
+                if(k!=0 && n%k != 0) {
+                    isDivisible = false;
+                    break;
+                }
+            }
+            if(isDivisible) {
+                validNum = n;
+                break;
+            }
+            n += 1;
+        }
+        out.println(validNum);
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+} 
