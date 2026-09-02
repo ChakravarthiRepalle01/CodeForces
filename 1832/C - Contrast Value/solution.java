c39557be4a23e4bb21f6b4d0910c1fe69d6d291f
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-02 22:05:50 +0530

    Add Contrast Value [C] from Codeforces

---
+++ 1832/C - Contrast Value/solution.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = 1;
+        t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+ 
+        int arr[] = new int[n];
+ 
+        for(int i = 0 ; i<n ; i++) arr[i] = in.nextInt();
+ 
+        int count = 1;
+        int dir = 0;
+ 
+        for(int i = 1 ; i<n ; i++) {
+            if(arr[i] > arr[i-1] && dir!=1) {
+                count++;
+                dir = 1;
+            }
+            else if(arr[i] < arr[i-1] && dir!=-1) {
+                count++;
+                dir = -1;
+            }
+        }
+ 
+        out.println(count);
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+} 
