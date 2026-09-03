cde3cde315f45074a7c031d1de683bd44ea9000a
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-03 21:01:35 +0530

    Add WOW Factor [B] from Codeforces

---
+++ 1178/B - WOW Factor/solution.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = 1;
+        // t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        String s = in.next();
+        int n = s.length();
+ 
+        long w = 0;
+        long wo = 0;
+        long wow = 0;
+ 
+        for(int i = 1 ; i<n ; i++) {
+            if(s.charAt(i) == 'v') {
+                if(s.charAt(i-1) == 'v') {
+                    w += 1;
+                    wow += wo;
+                }
+            }
+            else {
+                wo += w;
+            }
+        }
+ 
+        out.println(wow);
+ 
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+} 
