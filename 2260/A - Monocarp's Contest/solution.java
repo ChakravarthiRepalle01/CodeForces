acbe1c5f7f3676564521f3fb476552f6576b2f5a
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-08 20:38:53 +0530

    Add Monocarp's Contest [A] from Codeforces

---
+++ 2260/A - Monocarp's Contest/solution.java
@@ -0,0 +1,81 @@
+// _________ .__            __                                       __  .__    .__ 
+// \_   ___ \|  |__ _____  |  | ______________ ___  _______ ________/  |_|  |__ |__|
+// /    \  \/|  |  \\__  \ |  |/ /\_  __ \__  \\  \/ /\__  \\_  __ \   __\  |  \|  |
+// \     \___|   Y  \/ __ \|    <  |  | \// __ \\   /  / __ \|  | \/|  | |   Y  \  |
+//  \______  /___|  (____  /__|_ \ |__|  (____  /\_/  (____  /__|   |__| |___|  /__|
+//         \/     \/     \/     \/            \/           \/                 \/    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = 1;
+        t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        
+        int n = in.nextInt();
+ 
+        int nums[] = new int[n];
+ 
+        int z = 0;
+ 
+        for(int i = 0 ; i<n ; i++)  {
+            nums[i] = in.nextInt();
+            if(nums[i] == 0) z++;
+        }
+ 
+        if(z < 2) out.println(-1);
+        else {
+            if(nums[0] == 0 && nums[n-1] == 0) {
+                out.println(0);
+            }
+            else if(nums[0] == 0 || nums[n-1] == 0) {
+                out.println(1);
+            }
+            else {
+                out.println(2);
+            }
+        }
+ 
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+} 
