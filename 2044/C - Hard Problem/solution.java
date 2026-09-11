63def7f0ddc2592430bd9201980513391c1d83d8
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-11 16:35:46 +0530

    Add Hard Problem [C] from Codeforces

---
+++ 2044/C - Hard Problem/solution.java
@@ -0,0 +1,74 @@
+// _________ .__            __                                       __  .__    .__ 
+// \_   ___ \|  |__ _____  |  | ______________ ___  _______ ________/  |_|  |__ |__|
+// /    \  \/|  |  \\__  \ |  |/ /\_  __ \__  \\  \/ /\__  \\_  __ \   __\  |  \|  |
+// \     \___|   Y  \/ __ \|    <  |  | \// __ \\   /  / __ \|  | \/|  | |   Y  \  |
+//  \______  /___|  (____  /__|_ \ |__|  (____  /\_/  (____  /__|   |__| |___|  /__|
+//         \/     \/     \/     \/            \/           \/                 \/    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = 1;
+        t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int m = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+ 
+        int totalMonkeys = 0;
+ 
+        //row1
+        totalMonkeys += Math.min(a,m);
+        int rem1 = m-Math.min(a,m);
+ 
+        //row2
+        totalMonkeys += Math.min(m,b);
+        int rem2 = m-Math.min(m,b);
+ 
+        totalMonkeys += Math.min(c , rem1+rem2);
+ 
+        out.println(totalMonkeys);
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
