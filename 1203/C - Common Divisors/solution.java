4860edd42e612bedcbf567a077251e731f4a52ff
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-16 22:05:59 +0530

    Add Common Divisors [C] from Codeforces

---
+++ 1203/C - Common Divisors/solution.java
@@ -0,0 +1,82 @@
+// _________ .__            __                                       __  .__    .__ 
+// \_   ___ \|  |__ _____  |  | ______________ ___  _______ ________/  |_|  |__ |__|
+// /    \  \/|  |  \\__  \ |  |/ /\_  __ \__  \\  \/ /\__  \\_  __ \   __\  |  \|  |
+// \     \___|   Y  \/ __ \|    <  |  | \// __ \\   /  / __ \|  | \/|  | |   Y  \  |
+//  \______  /___|  (____  /__|_ \ |__|  (____  /\_/  (____  /__|   |__| |___|  /__|
+//         \/     \/     \/     \/            \/           \/                 \/    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = 1;
+        // t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+        
+        long GCD = 0;
+ 
+        for(int i = 0 ; i<n ; i++) {
+            long k = in.nextLong();
+            GCD = getGCD(k , GCD);
+        }
+ 
+        long totalCnt = 0;
+ 
+        for(long i = 1 ; i*i<=GCD ; i++) {
+            if(GCD%i == 0) {
+                totalCnt++;
+                if(GCD/i != i) {
+                    totalCnt++;
+                }
+            }
+        }
+ 
+        out.println(totalCnt);
+    }
+ 
+    public static long getGCD(long a , long b) {
+        if(b == 0) return a;
+        return getGCD(b , a%b);
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
