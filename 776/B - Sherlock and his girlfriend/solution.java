a161f50961740fea5b0ea75b613514e0e411288b
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-26 21:22:39 +0530

    Add Sherlock and his girlfriend [B] from Codeforces

---
+++ 776/B - Sherlock and his girlfriend/solution.java
@@ -0,0 +1,83 @@
+// _________ .__            __                                       __  .__    .__ 
+// \_   ___ \|  |__ _____  |  | ______________ ___  _______ ________/  |_|  |__ |__|
+// /    \  \/|  |  \\__  \ |  |/ /\_  __ \__  \\  \/ /\__  \\_  __ \   __\  |  \|  |
+// \     \___|   Y  \/ __ \|    <  |  | \// __ \\   /  / __ \|  | \/|  | |   Y  \  |
+//  \______  /___|  (____  /__|_ \ |__|  (____  /\_/  (____  /__|   |__| |___|  /__|
+//         \/     \/     \/     \/            \/           \/                 \/    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = 1;
+        // t = in.nextInt();
+        while (t > 0) {
+            solve(in, out);
+            t--;
+        }
+ 
+        out.flush();
+    }
+ 
+    public static void solve(FastScanner in, PrintWriter out) {
+        int n = in.nextInt();
+ 
+        boolean primes[] = new boolean[n+2];
+        
+        for(int i = 2 ; i*i <= (n+1) ; i++) {
+            for(int j = i*i ; j<=(n+1) ; j+=i) {
+                primes[j] = true;
+            }
+        }
+ 
+        if(n>=3) {
+            out.println(2);
+        }
+        else {
+            out.println(1);
+        }
+ 
+        for(int i = 2 ; i<=(n+1) ; i++) {
+            if(primes[i]) {
+                out.print(2+ " ");
+            }
+            else {
+                out.print(1+ " ");
+            }
+        }
+ 
+        out.println();
+    }
+ 
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { 
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
